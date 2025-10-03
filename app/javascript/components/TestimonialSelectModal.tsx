2aafbec6f523c5d9d4a3e09b1bb18fe9c051a00d
praveenkalady <praveenkaladytech@gmail.com>
2025-10-03 22:47:42 +0530

    Convert inline styles to tailwind for TestimonialsSelectModal

--- app/javascript/components/TestimonialSelectModal.tsx
+++ app/javascript/components/TestimonialSelectModal.tsx
@@ -9,6 +9,7 @@ import { Modal } from "$app/components/Modal";
 import { PaginationProps } from "$app/components/Pagination";
 import { Review } from "$app/components/Review";
 import { showAlert } from "$app/components/server-components/Alert";
+import cx from "classnames";
 
 export const TestimonialSelectModal = ({
   isOpen,
@@ -129,7 +130,7 @@ export const TestimonialSelectModal = ({
               />
               <p>Select all</p>
             </div>
-            <section className="paragraphs" style={{ marginTop: "var(--spacer-2)" }}>
+            <section className={cx("paragraphs", "mt-[var(--spacer-2)]")}>
               {state.reviews.map((review) => (
                 <SelectableReviewCard
                   key={review.id}
